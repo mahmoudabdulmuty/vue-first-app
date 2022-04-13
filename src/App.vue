83655dfbc8da871ecfbdc6ea9bb9bbe71fa0ade6
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-04-13 09:02:01 +0200

    created a basic vue app and added a Component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <section>
+    <h2>Friend Contact</h2>
+    <ul>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      friends: [
+        {
+          id: 1,
+          username: "Mahmoud Abdulmuty",
+          phone: "4821 4874 541",
+          email: "Mahmoud@example.com",
+        },
+        {
+          id: 2,
+          username: "Yusuf Saied",
+          phone: "2482 487544 541",
+          email: "Yusuf@example.com",
+        },
+        {
+          id: 3,
+          username: "Ahmed Salman",
+          phone: "4821 7544 145",
+          email: "Ahmed@example.com",
+        },
+      ],
+    };
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,37 @@
+<template>
+  <li>
+    <h2>{{ friend.username }}</h2>
+    <button @click="toggleDetails">{{ detailsValue }} Details</button>
+    <ul v-if="dataIsVisible">
+      <li><strong>Phone : </strong> {{ friend.phone }}</li>
+      <li><strong>Email : </strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      dataIsVisible: false,
+      friend: {
+        id: 1,
+        username: "Mahmoud Abdulmuty",
+        phone: "4821 4874 541",
+        email: "Mahmoud@example.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.dataIsVisible = !this.dataIsVisible;
+    },
+  },
+  computed: {
+    detailsValue() {
+      return this.dataIsVisible ? "Hide" : "Show";
+    },
+  },
+};
+</script>
